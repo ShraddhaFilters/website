430feae7ef09e33ae9d16a2335c5086c8a3a025d
ShraddhaFilters <shraddhafilters@gmail.com>
2025-06-15 16:17:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Fallback route to index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/homepage.html'));
 });
 
 // Homepage
